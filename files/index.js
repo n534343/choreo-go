e41592efee7173fc67ee920de78a8c770eb6a8be
back4app-12 <147476985+n534343@users.noreply.github.com>
2024-06-17 16:34:14 +1200

    Update index.js

--- files/index.js
+++ files/index.js
@@ -24,10 +24,10 @@ const ISP = metaInfo.trim();
 
 // sub subscription
 app.get('/sub2', (req, res) => {
-  const VMESS = { v: '2', ps: `${NAME}-${ISP}`, add: CFIP, port: '443', id: UUID, aid: '0', scy: 'none', net: 'ws', type: 'none', host: AG_DOMAIN, path: '/vmess?ed=2048', tls: 'tls', sni: AG_DOMAIN, alpn: '' };
-  const vlessURL = `vless://${UUID}@${CFIP}:443?encryption=none&security=tls&sni=${AG_DOMAIN}&type=ws&host=${AG_DOMAIN}&path=%2Fvless?ed=2048#${NAME}-${ISP}`;
+  const VMESS = { v: '2', ps: `${NAME}-${ISP}`, add: CFIP, port: '443', id: UUID, aid: '0', scy: 'none', net: 'ws', type: 'none', host: AG_DOMAIN, path: '/vmess-2024?ed=2048', tls: 'tls', sni: AG_DOMAIN, alpn: '' };
+  const vlessURL = `vless://${UUID}@${CFIP}:443?encryption=none&security=tls&sni=${AG_DOMAIN}&type=ws&host=${AG_DOMAIN}&path=%2Fvless-2024?ed=2048#${NAME}-${ISP}`;
   const vmessURL = `vmess://${Buffer.from(JSON.stringify(VMESS)).toString('base64')}`;
-  const trojanURL = `trojan://${UUID}@${CFIP}:443?security=tls&sni=${AG_DOMAIN}&type=ws&host=${AG_DOMAIN}&path=%2Ftrojan?ed=2048#${NAME}-${ISP}`;
+  const trojanURL = `trojan://${UUID}@${CFIP}:443?security=tls&sni=${AG_DOMAIN}&type=ws&host=${AG_DOMAIN}&path=%2Ftrojan-2024?ed=2048#${NAME}-${ISP}`;
   
   const base64Content = Buffer.from(`${vlessURL}\n\n${vmessURL}\n\n${trojanURL}`).toString('base64');
 
